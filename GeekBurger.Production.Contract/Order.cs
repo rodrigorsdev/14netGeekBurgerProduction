77ccaf0e2531206ca420a528c092789c935c5e8f
logonrmlocal <logonrmlocal@L2201MICRO22>
2019-06-13 21:24:18 -0300

    Updating nuget reference

--- GeekBurger.Production.Contract/Order.cs
+++ GeekBurger.Production.Contract/Order.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-//using GeekBurger.StoreCatalog.Contract;
+using GeekBurger.Products.Contract;
 
 namespace GeekBurger.Production.Contract
 {
@@ -12,8 +12,7 @@ namespace GeekBurger.Production.Contract
         public Guid StoreId { get; set; }
         public decimal Total { get; set; }
 
-        //TODO: Add reference to StoreCatalog
-        //public ICollection<ProductToGet> Products { get; set; }
+        public ICollection<ProductToGet> Products { get; set; }
 
         public ICollection<Guid> ProductionIds { get; set; }
         public string State { get; set; } //Paid, Canceled, Finished

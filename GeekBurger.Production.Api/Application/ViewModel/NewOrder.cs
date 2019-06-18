ec47ba49fbd821af7e90327c0bfffc18a7b82fb4
Rodrigo Oliveira <rodrigo.soliveira2322@gmail.com>
2019-06-18 20:28:48 -0300

    OrderChanded event criado

---
+++ GeekBurger.Production.Api/Application/ViewModel/NewOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Production.Application.ViewModel
+{
+    public class NewOrder
+    {
+        public Guid OrderId { get; set; }
+        public Guid StoreId { get; set; }
+        public decimal Total { get; set; }
+        public ICollection<NewOrderProduct> Products { get; set; }
+        public ICollection<Guid> ProductionIds { get; set; }
+    }
+}
---
+++ GeekBurger.Production.Api/Application/ViewModel/NewOrderProduct.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GeekBurger.Production.Application.ViewModel
+{
+    public class NewOrderProduct
+    {
+        public Guid ProductId { get; set; }
+    }
+}
